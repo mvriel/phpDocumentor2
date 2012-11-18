2b44a3a008493c9a283523436ef8bc64c88daf94
Mike van Riel <mike.vanriel@naenius.com>
2012-11-18 10:32:33 +0100

    Use XMLWriter to write contents
    During a discussion in #648 has @boenrobot indicated that XMLWriter can be
    significantly faster and less memory intensive than DOMDocument. In this
    commit I have started to implement a prototype of the new AST with XMLWriter
    support to test this approach and have an example implementation.

--- src/phpDocumentor/Parser/Exporter/Ast/Ast.php
+++ src/phpDocumentor/Parser/Exporter/Ast/Ast.php
@@ -4,7 +4,6 @@
  *
  * PHP Version 5.3
  *
- * @author    Mike van Riel <mike.vanriel@naenius.com>
  * @copyright 2010-2012 Mike van Riel / Naenius (http://www.naenius.com)
  * @license   http://www.opensource.org/licenses/mit-license.php MIT
  * @link      http://phpdoc.org
@@ -14,7 +13,11 @@ namespace phpDocumentor\Parser\Exporter\Ast;
 
 use phpDocumentor\Parser\Exporter\ExporterAbstract;
 use phpDocumentor\Reflection\FileReflector;
-use phpDocumentor\Reflection\ClassReflector;
+use phpDocumentor\Reflection\InterfaceReflector;
+use phpDocumentor\Reflection\ConstantReflector;
+use phpDocumentor\Reflection\FunctionReflector;
+use phpDocumentor\Reflection\ClassReflector\PropertyReflector;
+use phpDocumentor\Reflection\ClassReflector\MethodReflector;
 use phpDocumentor\Reflection\DocBlock;
 
 class Ast extends ExporterAbstract
@@ -29,16 +32,16 @@ class Ast extends ExporterAbstract
      */
     public function initialize()
     {
-        $this->xml = new \DOMDocument('1.0', 'utf-8');
-        $this->xml->formatOutput = true;
-
-        $document_element = new \DOMElement('project');
-        $this->xml->appendChild($document_element);
-
-        $document_element->setAttribute('version', '1.0.0');
-
-        $name = new \DOMElement('name', $this->parser->getTitle());
-        $document_element->appendChild($name);
+//        $this->xml = new \DOMDocument('1.0', 'utf-8');
+//        $this->xml->formatOutput = true;
+//
+//        $document_element = new \DOMElement('project');
+//        $this->xml->appendChild($document_element);
+//
+//        $document_element->setAttribute('version', '1.0.0');
+//
+//        $name = new \DOMElement('name', $this->parser->getTitle());
+//        $document_element->appendChild($name);
     }
 
     /**
@@ -50,31 +53,159 @@ class Ast extends ExporterAbstract
      */
     public function export($file)
     {
-        $file_node = new \DOMElement('file');
-        $this->xml->documentElement->appendChild($file_node);
-        $file_node->appendChild(
-            new \DOMElement('name', basename($file->getFilename()))
-        );
-        $this->exportDocBlock($file_node, $file->getDocBlock());
-        $file_node->appendChild(new \DOMElement('path', $file->getFilename()));
+        $writer = new \XMLWriter();
+        $writer->openMemory();
+        $writer->setIndent(true);
+        $writer->startDocument('1.0', 'utf-8');
+        $writer->startElement('file');
 
-        foreach($file->getNamespaceAliases() as $alias => $namespace) {
-            $alias_node = new \DOMElement('namespace-alias');
-            $file_node->appendChild($alias_node);
+        $writer->writeElement('name', basename($file->getFilename()));
+        $writer->writeElement('path', $file->getFilename());
+        $this->exportDocBlock($writer, $file->getDocBlock());
 
-            $alias_node->appendChild(new \DOMElement('name', $alias));
-            $alias_node->appendChild(new \DOMElement('fqsen', $namespace));
+        foreach ($file->getNamespaceAliases() as $alias => $namespace) {
+            $writer->startElement('namespace_alias');
+            $writer->writeElement('name', $alias);
+            $writer->writeElement('fqsen', $namespace);
+            $writer->endElement();
         }
 
-        foreach($file->getClasses() as $class) {
-            $this->exportClass($file_node, $class);
+        $writer->endElement();
+        $writer->endDocument();
+
+        foreach ($file->getClasses() as $contents) {
+            $class_writer = new \XMLWriter();
+            $class_writer->openMemory();
+            $class_writer->setIndent(true);
+            $class_writer->startDocument('1.0', 'utf-8');
+
+            $this->exportClass($class_writer, $contents, $file->getFilename());
+
+            $class_writer->endDocument();
+            var_dump($class_writer->outputMemory());
         }
+
+        foreach ($file->getConstants() as $contents) {
+            $constant_writer = new \XMLWriter();
+            $constant_writer->openMemory();
+            $constant_writer->setIndent(true);
+            $constant_writer->startDocument('1.0', 'utf-8');
+
+            $this->exportConstant($constant_writer, $contents, $file->getFilename());
+
+            $constant_writer->endDocument();
+            var_dump($constant_writer->outputMemory());
+        }
+
+        foreach ($file->getFunctions() as $contents) {
+            $function_writer = new \XMLWriter();
+            $function_writer->openMemory();
+            $function_writer->setIndent(true);
+            $function_writer->startDocument('1.0', 'utf-8');
+
+            $this->exportFunction($function_writer, $contents, $file->getFilename());
+
+            $function_writer->endDocument();
+            var_dump($function_writer->outputMemory());
+        }
+
+//        var_dump($writer->outputMemory());
     }
 
-    protected function exportClass(
-        \DOMElement $file_node, ClassReflector $class
-    ) {
+    protected function exportClass(\XMLWriter $writer, InterfaceReflector $class, $filename)
+    {
+        $element_name = 'interface';
+        if ($class instanceof \phpDocumentor\Reflection\ClassReflector) {
+            $element_name = 'class';
+        }
+        if ($class instanceof \phpDocumentor\Reflection\TraitReflector) {
+            $element_name = 'trait';
+        }
 
+        $writer->startElement($element_name);
+
+        $writer->writeAttribute('final', var_export($class->isFinal(), true));
+        $writer->writeAttribute('abstract', var_export($class->isAbstract(), true));
+        $writer->writeAttribute('line_number', $class->getLinenumber());
+        $writer->writeAttribute('file_name', $filename);
+
+        $writer->writeElement('name', $class->getShortName());
+        $writer->writeElement('fqsen', $class->getName());
+        $this->exportDocBlock($writer, $class->getDocBlock());
+
+        foreach ($class->getConstants() as $constant) {
+            $this->exportConstant($writer, $constant);
+        }
+
+        foreach ($class->getProperties() as $property) {
+            $this->exportProperty($writer, $property);
+        }
+
+        foreach ($class->getMethods() as $method) {
+            $this->exportMethod($writer, $method);
+        }
+
+        $writer->endElement();
+    }
+
+    protected function exportConstant(\XMLWriter $writer, ConstantReflector $constant, $filename = null)
+    {
+        $writer->startElement('constant');
+
+        $writer->writeAttribute('line_number', $constant->getLinenumber());
+        if ($filename) {
+            $writer->writeAttribute('file_name', $filename);
+        }
+
+        $writer->writeElement('name', $constant->getName());
+        $writer->writeElement('fqsen', '');
+        $this->exportDocBlock($writer, $constant->getDocBlock());
+
+        $writer->endElement();
+    }
+
+    protected function exportProperty(\XMLWriter $writer, PropertyReflector $property)
+    {
+        $writer->startElement('property');
+
+        $writer->writeAttribute('static', var_export($property->isStatic(), true));
+        $writer->writeAttribute('line_number', $property->getLinenumber());
+
+        $writer->writeElement('name', $property->getName());
+        $writer->writeElement('fqsen', '');
+        $this->exportDocBlock($writer, $property->getDocBlock());
+
+        $writer->endElement();
+    }
+
+    protected function exportMethod(\XMLWriter $writer, MethodReflector $method)
+    {
+        $writer->startElement('method');
+
+        $writer->writeAttribute('static', var_export($method->isStatic(), true));
+        $writer->writeAttribute('final', var_export($method->isFinal(), true));
+        $writer->writeAttribute('abstract', var_export($method->isAbstract(), true));
+        $writer->writeAttribute('line_number', $method->getLinenumber());
+
+        $writer->writeElement('name', $method->getName());
+        $writer->writeElement('fqsen', '');
+        $this->exportDocBlock($writer, $method->getDocBlock());
+
+        $writer->endElement();
+    }
+
+    protected function exportFunction(\XMLWriter $writer, FunctionReflector $method, $filename)
+    {
+        $writer->startElement('function');
+
+        $writer->writeAttribute('file_name', $filename);
+        $writer->writeAttribute('line_number', $method->getLinenumber());
+
+        $writer->writeElement('name', $method->getName());
+        $writer->writeElement('fqsen', '');
+        $this->exportDocBlock($writer, $method->getDocBlock());
+
+        $writer->endElement();
     }
 
     /**
@@ -85,24 +216,23 @@ class Ast extends ExporterAbstract
      */
     public function getContents()
     {
-        return $this->xml->saveXML();
+//        return $this->xml->saveXML();
     }
 
-    protected function exportDocBlock(
-        \DOMElement $element, DocBlock $docblock = null
-    ) {
+    protected function exportDocBlock(\XMLWriter $writer, DocBlock $docblock = null)
+    {
         if (!$docblock) {
             return;
         }
 
-        $element->appendChild(
-            new \DOMElement('summary', $docblock->getShortDescription())
-        );
-        $element->appendChild(
-            new \DOMElement(
-                'description', $docblock->getLongDescription()->getContents()
-            )
-        );
+        $this->writeFormattedText($writer, 'summary', $docblock->getShortDescription());
+        $this->writeFormattedText($writer, 'description', $docblock->getLongDescription()->getContents());
     }
 
+    protected function writeFormattedText(\XMLWriter $writer, $name, $text)
+    {
+        $writer->startElement($name);
+        $writer->writeElementNs('docbook', 'para', null, $text);
+        $writer->endElement();
+    }
 }
