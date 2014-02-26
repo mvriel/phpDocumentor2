ae05a0abe5912b5b085cdde237c5f7de1143667d
Mike van Riel <me@mikevanriel.com>
2014-02-26 20:29:38 +0100

    #636: Started refactoring of client code generator

--- src/phpDocumentor/Plugin/Search/Client/Generator.php
+++ src/phpDocumentor/Plugin/Search/Client/Generator.php
@@ -13,15 +13,45 @@ class Generator
         $this->twig = $twig;
     }
 
-    public function generate(AdapterInterface $engine)
+    public function generate(AdapterInterface $engine, $type = 'backend')
+    {
+        $filename = $this->getTemplateFolder() . $this->findEngineType($engine) . '.' . $type . '.twig.php';
+        if (! file_exists($filename)) {
+            return '';
+        }
+
+        return $this->generateClientFromTemplateWithConfiguration($filename, $engine->getConfiguration());
+    }
+
+    /**
+     * @param $filename
+     * @param $configuration
+     * @return string
+     */
+    protected function generateClientFromTemplateWithConfiguration($filename, $configuration)
+    {
+        $parameters = array('configuration' => $configuration);
+        $this->twig->setLoader(new \Twig_Loader_String());
+
+        return $this->twig->render(file_get_contents($filename), $parameters);
+    }
+
+    /**
+     * @param AdapterInterface $engine
+     * @return mixed
+     */
+    protected function findEngineType(AdapterInterface $engine)
     {
         $class_name_parts = explode('\\', get_class($engine));
-        $engine_type = $class_name_parts[count($class_name_parts)-1];
+        $engine_type = $class_name_parts[count($class_name_parts) - 1];
+        return $engine_type;
+    }
 
-        $this->twig->setLoader(new \Twig_Loader_String());
-        return $this->twig->render(
-            file_get_contents(__DIR__.'/Templates/'.$engine_type.'.twig.php'),
-            array('configuration' => $engine->getConfiguration())
-        );
+    /**
+     * @return string
+     */
+    protected function getTemplateFolder()
+    {
+        return __DIR__ . '/Templates/';
     }
 }
