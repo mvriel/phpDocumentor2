676b08dbad74e706b55cd7002b2637f3265a94e5
Mike van Riel <me@mikevanriel.com>
2014-02-28 08:19:36 +0100

    #636: Add documentation for Document
    The document class did not feature any documentation. This commit adds
    the necessary DocBlocks to the entity.

--- src/phpDocumentor/Plugin/Search/Document.php
+++ src/phpDocumentor/Plugin/Search/Document.php
@@ -1,15 +1,31 @@
 <?php
 namespace phpDocumentor\Plugin\Search;
 
+/**
+ * Represents a single Document entity in Search Engine results.
+ */
 class Document extends \ArrayObject
 {
-    protected $id  = '';
+    /** @var string the unique identifier for this Search Document */
+    protected $id = '';
 
+    /**
+     * Sets the unique identifier for this Document.
+     *
+     * @param string $id
+     *
+     * @return void
+     */
     public function setId($id)
     {
         $this->id = $id;
     }
 
+    /**
+     * Returns the unique identifier for this Document.
+     *
+     * @return string
+     */
     public function getId()
     {
         return $this->id;
