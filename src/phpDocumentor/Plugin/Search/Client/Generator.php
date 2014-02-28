22cbf9362fa32ae60abafcec333279d045cd73fc
Mike van Riel <me@mikevanriel.com>
2014-02-28 08:10:51 +0100

    #636: Refactored Client Code Generator and added test
    The client code generator did not support creating multiple types of
    clients. Since most search engines will have a Javascript search option
    and a PHP (or other) server side script that communicates with that
    Search Engine the generator should be able to create frontend and
    backend code.

--- src/phpDocumentor/Plugin/Search/Client/Generator.php
+++ src/phpDocumentor/Plugin/Search/Client/Generator.php
@@ -1,57 +1,112 @@
 <?php
-namespace phpDocumentor\Search\Client;
+namespace phpDocumentor\Plugin\Search\Client;
 
 use \phpDocumentor\Plugin\Search\Adapter\AdapterInterface;
 
+/**
+ * Generates the code that is consumed by a third-party in order to invoke a generator.
+ *
+ * This class is used by, for example, templates to receive a specific piece of javascript or PHP code
+ * that can be used as a client and to interact with the associated engine.
+ */
 class Generator
 {
     /** @var \Twig_Environment */
     protected $twig;
 
+    /**
+     * Registers the Twig environment which is used to render the templates with.
+     *
+     * @param \Twig_Environment $twig
+     */
     public function __construct(\Twig_Environment $twig)
     {
         $this->twig = $twig;
     }
 
+    /**
+     * Renders the client code for an engine and returns it.
+     *
+     * This method allows the caller to generate a piece of client code that can be included, embedded or called in
+     * another page. Because most engines have their interfacing split between frontend code (usually Javascript) and
+     * a backend script that is invoked (usually PHP) this method can generate a specific type per engine.
+     *
+     * Specific engines may have types other than 'backend' and 'frontend', please review the documentation for that
+     * engine that you want to use for more information.
+     *
+     * @param AdapterInterface $engine The engine for which to create a client.
+     * @param string           $type   The type of client to receive, usually 'backend' or 'frontend'.
+     *
+     * @todo for next version: change twig handling from the string loader to the normal file loader so that we can
+     *   pass alternate template locations to this object and thus allow plugins to add their own templates.
+     *
+     * @return string the client code or an empty string if the given type is not supported for this engine.
+     */
     public function generate(AdapterInterface $engine, $type = 'backend')
     {
-        $filename = $this->getTemplateFolder() . $this->findEngineType($engine) . '.' . $type . '.twig.php';
-        if (! file_exists($filename)) {
+        return $this->generateClientFromTemplateWithConfiguration(
+            $this->getTemplateFolder() . $this->getTemplateFilename($engine, $type),
+            $engine->getConfiguration()
+        );
+    }
+
+    /**
+     * Returns a rendered client using the provided template and configuration.
+     *
+     * This method will pass the provided file's contents through Twig with the configuration provided as a global
+     * variable called 'configuration'. The result of this rendering is returned.
+     *
+     * @param string $templatePath  The location of the template.
+     * @param object $configuration
+     *
+     * @return string
+     */
+    protected function generateClientFromTemplateWithConfiguration($templatePath, $configuration)
+    {
+        if (! file_exists($templatePath)) {
             return '';
         }
 
-        return $this->generateClientFromTemplateWithConfiguration($filename, $engine->getConfiguration());
-    }
-
-    /**
-     * @param $filename
-     * @param $configuration
-     * @return string
-     */
-    protected function generateClientFromTemplateWithConfiguration($filename, $configuration)
-    {
-        $parameters = array('configuration' => $configuration);
         $this->twig->setLoader(new \Twig_Loader_String());
 
-        return $this->twig->render(file_get_contents($filename), $parameters);
+        return $this->twig->render(file_get_contents($templatePath), array('configuration' => $configuration));
     }
 
     /**
-     * @param AdapterInterface $engine
-     * @return mixed
-     */
-    protected function findEngineType(AdapterInterface $engine)
-    {
-        $class_name_parts = explode('\\', get_class($engine));
-        $engine_type = $class_name_parts[count($class_name_parts) - 1];
-        return $engine_type;
-    }
-
-    /**
+     * Returns the location of the client templates.
+     *
      * @return string
      */
     protected function getTemplateFolder()
     {
         return __DIR__ . '/Templates/';
+    }
+
+    /**
+     * Returns the filename for a template based on the provided engine and type.
+     *
+     * @param AdapterInterface $engine
+     * @param string           $type
+     *
+     * @return string
+     */
+    protected function getTemplateFilename(AdapterInterface $engine, $type)
+    {
+        return strtolower($this->findEngineType($engine) . '.' . $type . '.twig');
+    }
+
+    /**
+     * Attempts to discern the type of Engine based on its class name.
+     *
+     * @param AdapterInterface $engine
+     *
+     * @return string
+     */
+    protected function findEngineType(AdapterInterface $engine)
+    {
+        $class_name_parts = explode('\\', get_class($engine));
+        $engine_type = $class_name_parts[count($class_name_parts) - 1];
+
+        return $engine_type;
     }
 }
---
+++ src/phpDocumentor/Plugin/Search/Tests/Client/GeneratorTest.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace phpDocumentor\Plugin\Search\Tests\Client;
+
+use Mockery as m;
+use phpDocumentor\Plugin\Search\Adapter\ElasticSearch;
+use phpDocumentor\Plugin\Search\Client\Generator;
+
+/**
+ * Tests for the phpDocumentor\Plugin\Search\Client\Generator class.
+ */
+class GeneratorTest extends \PHPUnit_Framework_TestCase
+{
+    /** @var Generator */
+    private $fixture;
+
+    /** @var \Twig_Environment|m\MockInterface */
+    private $twigMock;
+
+    /**
+     * Initializes the fixture and Twig dependency.
+     */
+    protected function setUp()
+    {
+        $this->twigMock = m::mock('Twig_Environment');
+        $this->fixture  = new Generator($this->twigMock);
+    }
+
+    /**
+     * @covers phpDocumentor\Plugin\Search\Client\Generator::__construct
+     */
+    public function testIfDependenciesAreRegisteredUponInitialization()
+    {
+        $this->assertAttributeEquals($this->twigMock, 'twig', $this->fixture);
+    }
+
+    /**
+     * @covers phpDocumentor\Plugin\Search\Client\Generator::generate
+     * @covers phpDocumentor\Plugin\Search\Client\Generator::generateClientFromTemplateWithConfiguration
+     * @covers phpDocumentor\Plugin\Search\Client\Generator::getTemplateFolder
+     * @covers phpDocumentor\Plugin\Search\Client\Generator::getTemplateFilename
+     * @covers phpDocumentor\Plugin\Search\Client\Generator::findEngineType
+     */
+    public function testIfBackendClientCodeIsGenerated()
+    {
+        // Arrange
+        $type        = 'backend';
+        $expected    = 'abc';
+        $adapterMock = $this->givenAnElasticSearchEngine();
+        $this->thenTwigShouldRenderUsingTheConfiguration($adapterMock->getConfiguration(), $expected);
+
+        // Act
+        $result = $this->fixture->generate($adapterMock, $type);
+
+        // Assert
+        $this->assertInternalType('string', $result);
+        $this->assertSame($expected, $result);
+    }
+
+    /**
+     * @covers phpDocumentor\Plugin\Search\Client\Generator::generate
+     * @covers phpDocumentor\Plugin\Search\Client\Generator::generateClientFromTemplateWithConfiguration
+     */
+    public function testIfClientWithUnknownTypeReturnsAnEmptyString()
+    {
+        // Arrange
+        $type        = '_unknownType';
+        $adapterMock = $this->givenAnElasticSearchEngine();
+
+        // Act
+        $result = $this->fixture->generate($adapterMock, $type);
+
+        // Assert
+        $this->assertInternalType('string', $result);
+        $this->assertEmpty($result);
+    }
+
+    /**
+     * Instantiates an ElasticSearch adapter with a mocked configuration and returns those.
+     *
+     * @return ElasticSearch
+     */
+    private function givenAnElasticSearchEngine()
+    {
+        $configurationClass = 'phpDocumentor\Plugin\Search\Adapter\Configuration\ElasticSearch';
+        $configurationMock = m::mock($configurationClass)->shouldIgnoreMissing();
+        $adapterMock = new ElasticSearch($configurationMock);
+        return $adapterMock;
+    }
+
+    /**
+     * Configures the Twig mock to receive a setLoader and render call and return the expected value.
+     *
+     * @param m\MockInterface $configurationMock
+     * @param string          $expected
+     *
+     * @return void
+     */
+    private function thenTwigShouldRenderUsingTheConfiguration($configurationMock, $expected)
+    {
+        $this->twigMock->shouldReceive('setLoader')->andReturnNull();
+        $this->twigMock->shouldReceive('render')
+            ->with(m::type('string'), array('configuration' => $configurationMock))->andReturn($expected);
+    }
+}
